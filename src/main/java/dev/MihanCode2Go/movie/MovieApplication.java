0af7a643a53e015d4f26a520e47113bd492828bb
Mihan786Chistie <tubbachistie@gmail.com>
2023-02-28 14:14:27 +0530

    Changes in Application Properties

--- src/main/java/dev/MihanCode2Go/movie/MovieApplication.java
+++ src/main/java/dev/MihanCode2Go/movie/MovieApplication.java
@@ -2,20 +2,14 @@ package dev.MihanCode2Go.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MovieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String apiRoot(){
-		return "Hello World";
-	}
+
 
 }
